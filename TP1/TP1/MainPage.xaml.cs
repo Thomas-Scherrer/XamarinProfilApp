13577c80d03e2452060ba9fffe21a5385b7f1d44
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-03-04 21:37:05 +0100

    init TP1 Xamarin

---
+++ TP1/TP1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TP1
+{
+    public partial class MainPage : ContentPage
+    {
+        int i = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void HandleClick(object sender, EventArgs e)
+        {
+            i++;
+            Share.Text = i.ToString();
+        }
+    }
+}
